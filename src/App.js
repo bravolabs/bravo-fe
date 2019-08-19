ed32a76dd338f13d1bd9b4c184357f0b846db761
Noble Obioma <nobioma1@gmail.com>
2019-08-19 12:56:09 +0100

    Create Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+
 const App = () => (
-  <div>Welcome to Bravo-FE</div>
+  <div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route render={() => <h3>Route does not exist</h3>} />
+    </Switch>
+  </div>
 );
 
 export default App;
