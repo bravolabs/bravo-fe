20eb129d6439b555a40db34859fbb2a7dc0c3c00
Noble Obioma <nobioma1@gmail.com>
2019-08-19 12:40:26 +0100

    Create root reducer to combine all reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+import reducer from './reducer';
+
+const rootReducer = combineReducers({
+  /* Reducers */
+  reducer,
+});
+
+export default rootReducer;
