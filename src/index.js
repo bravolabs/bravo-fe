dc99c552725342ffd27ea1bacc35894edf0b3595
Noble Obioma <nobioma1@gmail.com>
2019-08-19 12:44:19 +0100

    Add Browser Router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store/configStore';
@@ -7,7 +8,9 @@ import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root'),
 );
