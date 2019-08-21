16a84b12ceb706766c01022bd1cf1cdffe67e9bb
ropeks <ropeksmilioner@gmail.com>
2019-08-21 16:48:31 +0200

    migrate home-page to react app: utils and html

--- src/components/Home/home.styles.js
+++ src/components/Home/home.styles.js
@@ -1,5 +1,63 @@
 import styled from 'styled-components';
 
-export const Text = styled.h1`
-  color: blue;
+export const Container = styled.div`
+  margin: auto;
+  max-width: 1440px;
+  max-height: 1000px;
+`;
+
+export const Nav = styled.nav`
+  
+`;
+
+export const LogoContainer = styled.div`
+  
+`;
+
+export const Logo = styled.img`
+  
+`;
+
+export const NavText = styled.span`
+  
+`;
+
+export const Main = styled.div`
+  
+`;
+
+export const Intro = styled.div`
+  
+`;
+
+export const Caption = styled.span`
+  
+`;
+
+export const IntroText = styled.span`
+  
+`;
+
+export const Cards = styled.div`
+  
+`;
+
+export const Card = styled.div`
+  
+`;
+
+export const Icon = styled.img`
+  
+`;
+
+export const Title = styled.span`
+  
+`;
+
+export const CardText = styled.span`
+  
+`;
+
+export const Footer = styled.div`
+  
 `;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,68 @@
 import React from 'react';
 
-import { Text } from './home.styles';
+// import styles
+import { 
+  Container, 
+  Nav, 
+  LogoContainer, 
+  Logo, 
+  NavText, 
+  Main, 
+  Intro, 
+  Caption, 
+  IntroText, 
+  Cards, 
+  Card, 
+  Icon, 
+  Title, 
+  CardText, 
+  Footer
+} from './home.styles';
 
+// import utils
+import logoImg from './utils/bravo-logo.png';
+import clapIcon from './utils/clap-icon.png';
+import giftIcon from './utils/gift-icon.png';
+import messageIcon from './utils/message-icon.png';
+
+// still have to make slack button a separate component
 const Home = () => (
-  <Text>
-    Welcome to Bravo-FE
-  </Text>
+  <Container>
+    <Nav>
+      <LogoContainer>
+        <Logo src={logoImg} />
+        <NavText>bravo</NavText>
+      </LogoContainer>
+      <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=733322040912.733329316565"><img src="https://platform.slack-edge.com/img/sign_in_with_slack.png" /></a>
+    </Nav>
+    <Main>
+      <Intro>
+        <Caption>when the job is done, say bravo</Caption>
+        <IntroText>bravo enables team members to easily acknowledge each other with one slack command</IntroText>
+        <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=733322040912.733329316565"><img src="https://platform.slack-edge.com/img/sign_in_with_slack.png" /></a>
+      </Intro>
+      <Cards>
+        <Card>
+          <Icon src={clapIcon} />
+          <Title>bravo slack bot</Title>
+          <CardText>install our slack integration in few simple clicks</CardText>
+        </Card>
+        <Card>
+          <Icon src={giftIcon} />
+          <Title>give a bravo</Title>
+          <CardText>select receiver, write your text, send</CardText>
+        </Card>
+        <Card>
+          <Icon src={messageIcon} />
+          <Title>receive a bravo</Title>
+          <CardText>get notification when someone sends you a bravo</CardText>
+        </Card>
+      </Cards>
+    </Main>
+    <Footer>
+
+    </Footer>
+  </Container>
 );
 
 export default Home;
