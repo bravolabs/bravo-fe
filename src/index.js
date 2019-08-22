d0ed20db51ef39497b50c55005c1c5fb41eb8d15
Noble Obioma <nobioma1@gmail.com>
2019-08-22 20:26:09 +0100

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store/configStore';
@@ -8,9 +7,7 @@ import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
