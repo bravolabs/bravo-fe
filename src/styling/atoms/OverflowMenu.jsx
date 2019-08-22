d682bceab08c6074f4362a91e69b03dba8e4904c
AaronJThompson <aaronjthompson98@gmail.com>
2019-08-22 21:23:11 +0100

    Add Overflow menu atoms

---
+++ src/styling/atoms/OverflowMenu.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const MenuContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  background: ${props => (props.background ? props.background : 'none')};
+  box-shadow: ${props => (props.shadow ? props.shadow : '0px 10px 20px rgba(0, 0, 0, 0.15)')};
+  position: absolute;
+  left: ${props => (props.x ? props.x : 0)};
+  top: ${props => (props.y ? props.y : 0)};
+`;
+
+export const MenuItem = styled.button`
+  border: none;
+  background: none;
+  padding: 16px 12px;
+  color: ${props => (props.color ? props.color : 'black')};
+  background: ${props => (props.background ? props.background : 'white')};
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-content: center;
+
+  & > * {
+    margin-right: 8px;
+  }
+
+  *:last-child {
+    margin-right: 0px;
+  }
+`;
