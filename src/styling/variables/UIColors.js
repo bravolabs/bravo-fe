0f97c2e7815c31ab9b26d0dec480e3315877e90b
AaronJThompson <aaronjthompson98@gmail.com>
2019-08-22 18:23:50 +0100

    Add folder structure and UIColors

---
+++ src/styling/variables/UIColors.js
@@ -0,0 +1,13 @@
+const Colors = {
+  Brand: '#4265ED',
+  Success: '#48D136',
+  Warning: '#FFA50A',
+  Error: '#FF513A',
+  Light: '#FAFCFD',
+  LightBorder: '#F1F2F6',
+  White: '#FFFFFF',
+  Darkener: 'rgba(0, 0, 0, 0.2)',
+  Lightener: 'rgba(255, 255, 255, 0.2)',
+};
+
+export default Colors;
