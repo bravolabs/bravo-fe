fb743648c88d44a6ff346ceb2574ecf558c85e40
AaronJThompson <aaronjthompson98@gmail.com>
2019-08-22 22:15:19 +0100

    Add TopNavigation attom

---
+++ src/styling/atoms/TopNavigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const NavigationContainer = styled.div`
+  box-sizing: border-box;
+  width: ${props => (props.width ? props.width : 0)};
+  border-bottom: ${props => (props.borderColor ? props.borderColor : 'white')};
+  color: ${props => (props.color ? props.color : 'black')};
+  padding-bottom: ${props => (props.bottomPadding ? props.bottomPadding : '16px')};
+`;
