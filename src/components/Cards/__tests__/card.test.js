b0bbff8b835f8021a8f49a329d748023f033bb14
Noble Obioma <nobioma1@gmail.com>
2019-08-26 17:40:54 +0100

    fix path in tests

--- src/components/Cards/__tests__/card.test.js
+++ src/components/Cards/__tests__/card.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 
-import Card from '../Card';
-import clapIcon from '../../../../assets/clap-icon.png';
+import HomeCard from '../HomeCard';
+import clapIcon from '../../../assets/clap-icon.png';
 
 afterEach(rtl.cleanup);
 describe('Cards Component', () => {
   it('Should render all cards on home', () => {
     const { getByText } = rtl.render(
-      <Card
+      <HomeCard
         icon={clapIcon}
         title="bravo slack bot"
         text="install our slack integration in few simple clicks"
--- src/components/Cards/__tests__/cards.test.js
+++ src/components/Cards/__tests__/cards.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 
-import Cards from '../index';
+import HomeCards from '../HomeCards';
 
 afterEach(rtl.cleanup);
 describe('Cards Component', () => {
   it('Should render all cards on home', () => {
-    const { getByText } = rtl.render(<Cards />);
+    const { getByText } = rtl.render(<HomeCards />);
     getByText('bravo slack bot');
     getByText('give a bravo');
     getByText('receive a bravo');
