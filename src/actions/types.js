136cc3405198a66b84c72cdac0d075084a7594d5
AaronJThompson <aaronjthompson98@gmail.com>
2019-08-28 13:32:05 +0100

    Add API action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,9 @@
+// API
+export const SET_API_ERROR = 'SET_API_ERROR';
+export const API_FETCHING = 'API_FETCHING';
+export const API_SUCCESS = 'API_SUCCESS';
+export const API_FAILURE = 'API_FAILURE';
+export const API_POSTING = 'API_POSTING';
+
+// Shoutouts
 export const SET_SINGLE_SHOUTOUT = 'SET_SINGLE_SHOUTOUT';
