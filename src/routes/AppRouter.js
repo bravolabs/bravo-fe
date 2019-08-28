46fa66b19fffee42bfe8ee01efc1425c5da3b1e3
ropeks <ropeksmilioner@gmail.com>
2019-08-28 15:23:42 +0200

    add /user route

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
 import Slack from '../components/Slack';
 import Shoutout from '../views/SingleShoutout';
+import UserProfileView from '../views/UserProfileView';
 
 const AppRouter = () => (
   <div>
@@ -11,6 +12,7 @@ const AppRouter = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/slack" component={Slack} />
       <Route exact path="/shoutout" component={Shoutout} />
+      <Route exact path="/user" component={UserProfileView} />
       <Route render={() => <h3>Heyo,This Route does not exist</h3>} />
     </Switch>
   </div>
