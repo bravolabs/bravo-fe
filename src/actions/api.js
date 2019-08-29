33536cdde0210285beb2393c12667fdede64d474
AaronJThompson <aaronjthompson98@gmail.com>
2019-08-29 08:07:43 +0100

    Add get and request handler

---
+++ src/actions/api.js
@@ -0,0 +1,33 @@
+import * as types from './types';
+import axios from 'axios';
+
+const baseURL = process.env.REACT_APP_API_HOST;
+
+const urlConstruct = extension => {
+  return `${baseURL}${extension}`;
+};
+
+const reqHandler = req => {
+  return req
+    .then(res => {
+      dispatch({ type: types.API_SUCCESS });
+      return res;
+    })
+    .catch(error => {
+      dispatch(setError(error.message));
+    });
+};
+
+export const setError = error => {
+  return {
+    type: types.SET_API_ERROR,
+    payload: error,
+  }
+};
+
+export const get = (path, auth) => dispatch => {
+  if (!auth) {
+    const req = axios.get(urlConstruct(path));
+    return reqHandler(req);
+  }
+};
