7ecf98c07d6b6dc8185a564af82c22969d6e732e
AaronJThompson <aaronjthompson98@gmail.com>
2019-08-30 05:42:02 +0100

    Add token on auth

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,5 +2,7 @@ import * as types from './types';
 import { post } from './api';
 
 export const login = (accessToken, userId) => dispatch => {
-  return dispatch(post('/api/auths', { accessToken, userId }));
+  return dispatch(post('/api/auths', { accessToken, userId })).then(res => {
+    localStorage.setItem('token', res.data.token);
+  });
 };
