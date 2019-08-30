f700f49d3d0c84bf9c91191a85d37957707d420b
Noble Obioma <nobioma1@gmail.com>
2019-08-30 13:38:14 +0100

    Create a ProtectedRoute HOC

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import localstorage from '../utils/localstorage';
+
+const ProtectedRoute = ({ component: Component, ...props }) => {
+  const token = localstorage.get();
+  return (
+    <Route
+      {...props}
+      render={() => {
+        if (token) {
+          return <Component />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: '/',
+                state: { from: props.location },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
