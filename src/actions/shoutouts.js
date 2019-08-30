711e47aaf3593fc1b08ee09ab68624b99eed0aa3
AaronJThompson <aaronjthompson98@gmail.com>
2019-08-30 05:51:29 +0100

    Add single shoutout actions

---
+++ src/actions/shoutouts.js
@@ -0,0 +1,15 @@
+import * as types from './types';
+import { fetch } from './api';
+
+export const setSingleShoutout = shoutout => {
+  return {
+    type: types.SET_SINGLE_SHOUTOUT,
+    payload: shoutout,
+  }
+}
+
+export const getSingleShoutout = id => dispatch => {
+  return dispatch(fetch('/api/shoutouts/' + id, true)).then(res => {
+    dispatch(setSingleShoutout(res.data));
+  });
+}
