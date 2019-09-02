91ace095e22ef578e448931de3a5e2c49af67e2c
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-02 14:49:46 +0100

    Add sign in redirect component

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from '../components/Home';
 import Slack from '../components/Slack';
+import SignInRedirect from '../components/SignInRedirect';
 import Shoutout from '../views/SingleShoutout';
 import { Redirect } from 'react-router-dom';
 import ProtectedRoute from '../components/ProtectedRoute';
@@ -11,6 +12,7 @@ const AppRouter = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/slack" component={Slack} />
+    <Route path="/slack-signin" component={SignInRedirect} />
     <ProtectedRoute path="/shoutout" component={Shoutout} />
     <Route render={() => <Redirect to="/" />} />
   </Switch>
