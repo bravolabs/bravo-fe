b116cfddc11fd0855e885be3b9ab69951261b42e
erozonachi <erozonachi.james@gmail.com>
2019-09-03 20:42:09 +0100

    Import and use member cards container
    Import and use member cards container styled component in member cards
    component.

--- src/components/Cards/MemberCards.js
+++ src/components/Cards/MemberCards.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import { MemberCardsContainer } from './cards.styles';
 
-const MemberCards = ({ members }) => <></>;
+const MemberCards = ({ members }) => <MemberCardsContainer></MemberCardsContainer>;
 
 export default MemberCards;
