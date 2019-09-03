ec3a8e5d870e7e0b73eae09607050c748cd25f8e
erozonachi <erozonachi.james@gmail.com>
2019-09-03 21:31:18 +0100

    Add and export team container styled component

---
+++ src/components/Team/team.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const TeamContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1000px;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  margin-top: 6rem;
+  margin-left: 20rem;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+`;
