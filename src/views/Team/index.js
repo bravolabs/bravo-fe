2cd5236f8f483b09ed4688835796b480d52e0660
erozonachi <erozonachi.james@gmail.com>
2019-09-03 22:59:45 +0100

    Import and use team in team view component

--- src/views/Team/index.js
+++ src/views/Team/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import SideNav from '../../components/SideNav';
+import Team from '../../components/Team/Team';
 
 const Team = () => (
   <>
     <SideNav />
+    <Team />
   </>
 );
 
