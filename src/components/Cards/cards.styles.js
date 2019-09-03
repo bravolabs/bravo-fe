5a7947004c7b17827bdd7749d999a636102ca825
erozonachi <erozonachi.james@gmail.com>
2019-09-03 14:20:08 +0100

    Import base button into cards styles file

--- src/components/Cards/cards.styles.js
+++ src/components/Cards/cards.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Card } from '../../styling/atoms/Card';
+import { BaseButton } from '../../styling/atoms/Buttons';
 import colors from '../../styling/variables/UIColors';
 
 export const CardsContainer = styled.div`
