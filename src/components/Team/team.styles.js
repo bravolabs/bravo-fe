699dbdcc34fdb7e00ccdb13798378dabb088860c
erozonachi <erozonachi.james@gmail.com>
2019-09-03 21:46:33 +0100

    Add and export title styled component

--- src/components/Team/team.styles.js
+++ src/components/Team/team.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { PageTitle } from '../../styling/atoms/Fonts';
 
 export const TeamContainer = styled.div`
   display: flex;
@@ -12,4 +13,12 @@ export const TeamContainer = styled.div`
   box-sizing: border-box;
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
+`;
+
+export const Title = styled(PageTitle)`
+  font-size: 3.2rem;
+  font-weight: 600;
+  line-height: 3.8rem;
+  color: #000;
+  margin-bottom: 4rem;
 `;
