87433c3159e7250f8fc7e68cfca5bdbd4afe9694
erozonachi <erozonachi.james@gmail.com>
2019-09-03 16:06:32 +0100

    Import and use view profile in member card

--- src/components/Cards/MemberCard.js
+++ src/components/Cards/MemberCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MemberCardContainer, MemberCardAction } from './cards.styles';
+import { MemberCardContainer, MemberCardAction, ViewProfile } from './cards.styles';
 import User from '../User';
 import placeholder from '../../assets/placeholder.png';
 
@@ -7,6 +7,7 @@ const MemberCard = ({ id, name, avatar }) => (
   <MemberCardContainer>
     <User img={avatar || placeholder} alt={name} name={name} />
     <MemberCardAction>···</MemberCardAction>
+    <ViewProfile>👀 View profile</ViewProfile>
   </MemberCardContainer>
 );
 
