9855a56c15e7c47aea172a6327db75259e9e716e
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-03 07:49:36 +0100

    Add user action creators

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,33 @@
+import { axiosWithAuth } from '../utils/axios';
+
 export const types = {
   ADD_USER_INFO: 'ADD_USER_INFO',
   FETCHING_USER: 'FETCHING_USER',
   USER_ERROR: 'USER_ERROR',
+};
+
+export const addUser = user => {
+  return {
+    type: types.ADD_USER_INFO,
+    payload: user,
+  };
+};
+
+export const setError = error => {
+  return {
+    type: types.USER_ERROR,
+    payload: error,
+  };
+};
+
+export const getUserInfo = id => dispatch => {
+  dispatch({ type: types.FETCHING_USER });
+  return axiosWithAuth
+    .get('/api/users/' + id)
+    .then(res => {
+      dispatch(addUser(res.data));
+    })
+    .catch(err => {
+      dispatch(setError(err.message));
+    });
 };
