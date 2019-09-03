85c68c01ca87a3b8970b76a0d7415b9c5d0a3183
erozonachi <erozonachi.james@gmail.com>
2019-09-03 14:37:58 +0100

    Import and use member card container
    Import and use member card container styled component in member card
    component.

--- src/components/Cards/MemberCard.js
+++ src/components/Cards/MemberCard.js
@@ -1,3 +1,4 @@
-const MemberCard = ({ id, name, avatar }) => <></>;
+import { MemberCardContainer } from './cards.styles';
+const MemberCard = ({ id, name, avatar }) => <MemberCardContainer></MemberCardContainer>;
 
 export default MemberCard;
