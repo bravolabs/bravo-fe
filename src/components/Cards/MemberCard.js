722d88b8346ce100fa7a3b4970469da6e5e482f4
erozonachi <erozonachi.james@gmail.com>
2019-09-03 14:50:48 +0100

    Import and use member card action in member card

--- src/components/Cards/MemberCard.js
+++ src/components/Cards/MemberCard.js
@@ -1,10 +1,11 @@
-import { MemberCardContainer } from './cards.styles';
+import { MemberCardContainer, MemberCardAction } from './cards.styles';
 import User from '../User';
 import placeholder from '../../assets/placeholder.png';
 
 const MemberCard = ({ id, name, avatar }) => (
   <MemberCardContainer>
     <User img={avatar || placeholder} alt={name} name={name} />
+    <MemberCardAction>···</MemberCardAction>
   </MemberCardContainer>
 );
 
