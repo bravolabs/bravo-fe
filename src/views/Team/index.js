fcb580265de73de806888c9293df3496da661b76
erozonachi <erozonachi.james@gmail.com>
2019-09-03 23:06:56 +0100

    Rename team to team view

--- src/views/Team/index.js
+++ src/views/Team/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import SideNav from '../../components/SideNav';
 import Team from '../../components/Team/Team';
 
-const Team = () => (
+const TeamView = () => (
   <>
     <SideNav />
     <Team />
   </>
 );
 
-export default Team;
+export default TeamView;
