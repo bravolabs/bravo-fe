856a29dd89432252d873b06c10ecf4b5462ba712
erozonachi <erozonachi.james@gmail.com>
2019-09-04 16:46:04 +0100

    Import and use loader, display-card in team component

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -4,6 +4,9 @@ import { TeamContainer, Title, TeamHead, HeadText } from './team.styles';
 import MemberCards from '../Cards/MemberCards';
 import { fetchTeamInfo } from '../../actions/team';
 
+import Loader from '../Loader';
+import DisplayCard from '../Cards/DisplayCard';
+
 const members = [
   {
     id: 1,
@@ -48,14 +51,17 @@ const members = [
 ];
 
 const Team = ({ team }) => (
-  <TeamContainer>
-    <Title>Team</Title>
-    <TeamHead>
-      <HeadText marginLeft={true}>Name</HeadText>
-      <HeadText>Actions</HeadText>
-    </TeamHead>
-    <MemberCards members={members} />
-  </TeamContainer>
+  <React.Fragment>
+    {team.isFetchingTeam && <DisplayCard header={<Loader />} text="Loading your Team..." />}
+    <TeamContainer>
+      <Title>Team</Title>
+      <TeamHead>
+        <HeadText marginLeft={true}>Name</HeadText>
+        <HeadText>Actions</HeadText>
+      </TeamHead>
+      <MemberCards members={members} />
+    </TeamContainer>
+  </React.Fragment>
 );
 
 export default connect(
