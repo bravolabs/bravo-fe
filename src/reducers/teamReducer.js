b40f57ebe82b76682e5a1a93ae3c2f9974706dd4
erozonachi <erozonachi.james@gmail.com>
2019-09-04 12:06:15 +0100

    Add initial state object to the team reducer

--- src/reducers/teamReducer.js
+++ src/reducers/teamReducer.js
@@ -1,1 +1,7 @@
 import { FETCHING_TEAM, TEAM_FETCH_SUCCESS, TEAM_FETCH_FAIL } from '../actions/team';
+
+const initialState = {
+  isFetchingTeam: false,
+  team: null,
+  fetchingTeamError: '',
+};
