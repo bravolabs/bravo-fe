919867105baa37494b23ddc6cf6321b4b85046e6
erozonachi <erozonachi.james@gmail.com>
2019-09-04 11:37:00 +0100

    Add and export bare bone of fetch team info dispatcher

--- src/actions/team.js
+++ src/actions/team.js
@@ -1,3 +1,9 @@
 export const FETCHING_TEAM = 'FETCHING_TEAM';
 export const TEAM_FETCH_SUCCESS = 'TEAM_FETCH_SUCCESS';
 export const TEAM_FETCH_FAIL = 'TEAM_FETCH_FAIL';
+
+export const fetchTeamInfo = organization => async dispatch => {
+  dispatch({ type: FETCHING_TEAM });
+  try {
+  } catch (error) {}
+};
