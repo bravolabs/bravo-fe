9aea57388b96691d84b21ac39ade242c09077ea9
erozonachi <erozonachi.james@gmail.com>
2019-09-04 18:13:28 +0100

    Import and apply use-effect hook in team component

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { TeamContainer, Title, TeamHead, HeadText } from './team.styles';
 import MemberCards from '../Cards/MemberCards';
@@ -8,24 +8,31 @@ import Loader from '../Loader';
 import DisplayCard from '../Cards/DisplayCard';
 import bravoParty from '../../assets/bravo-party.svg';
 
-const Team = ({ team, fetchTeamInfo }) => (
-  <React.Fragment>
-    {team.isFetchingTeam && <DisplayCard header={<Loader />} text="Loading your Team..." />}
-    {team.members && (
-      <TeamContainer>
-        <Title>Team</Title>
-        <TeamHead>
-          <HeadText marginLeft={true}>Name</HeadText>
-          <HeadText>Actions</HeadText>
-        </TeamHead>
-        <MemberCards members={team.members} />
-      </TeamContainer>
-    )}
-    {team.errorMessage && (
-      <DisplayCard header={<img src={bravoParty} alt="bravo party" />} text={team.errorMessage} />
-    )}
-  </React.Fragment>
-);
+const Team = ({ team, fetchTeamInfo }) => {
+  useEffect(() => {
+    if (team && !team.members) {
+      fetchTeamInfo('QHUESBNDGE');
+    }
+  }, []);
+  return (
+    <React.Fragment>
+      {team.isFetchingTeam && <DisplayCard header={<Loader />} text="Loading your Team..." />}
+      {team.members && (
+        <TeamContainer>
+          <Title>Team</Title>
+          <TeamHead>
+            <HeadText marginLeft={true}>Name</HeadText>
+            <HeadText>Actions</HeadText>
+          </TeamHead>
+          <MemberCards members={team.members} />
+        </TeamContainer>
+      )}
+      {team.errorMessage && (
+        <DisplayCard header={<img src={bravoParty} alt="bravo party" />} text={team.errorMessage} />
+      )}
+    </React.Fragment>
+  );
+};
 
 export default connect(
   state => ({ team: state.team }),
