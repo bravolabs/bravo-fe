9deab815859d5dbb76da73c93fe46e86dd6873ea
erozonachi <erozonachi.james@gmail.com>
2019-09-04 11:17:27 +0100

    Add authorization header to custome axios creator

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import localstorage from '../utils/localstorage';
+
+const { token } = localstorage.get();
 
 export const Axios = () => {
   return axios.create({
     headers: {
       'Content-Type': 'application/json',
+      Authorization: token,
     },
     baseURL: process.env.REACT_APP_API_HOST,
   });
