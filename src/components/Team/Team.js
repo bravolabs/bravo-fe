868f5395de1d799136028fa571e385ab7acb6611
erozonachi <erozonachi.james@gmail.com>
2019-09-04 16:56:20 +0100

    Remove members dummy data

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -8,49 +8,6 @@ import Loader from '../Loader';
 import DisplayCard from '../Cards/DisplayCard';
 import bravoParty from '../../assets/bravo-party.svg';
 
-const members = [
-  {
-    id: 1,
-    name: 'Samar Vir',
-    avatar: null,
-  },
-  {
-    id: 2,
-    name: 'Maxime Salomon',
-    avatar: null,
-  },
-  {
-    id: 3,
-    name: 'Aaron Thompson',
-    avatar: null,
-  },
-  {
-    id: 4,
-    name: 'Borja Soler',
-    avatar: null,
-  },
-  {
-    id: 5,
-    name: 'Johnson Ogwuru',
-    avatar: null,
-  },
-  {
-    id: 6,
-    name: 'Noble Obioma',
-    avatar: null,
-  },
-  {
-    id: 7,
-    name: 'Petar Vlaisavljevic',
-    avatar: null,
-  },
-  {
-    id: 8,
-    name: 'James Eneh',
-    avatar: null,
-  },
-];
-
 const Team = ({ team }) => (
   <React.Fragment>
     {team.isFetchingTeam && <DisplayCard header={<Loader />} text="Loading your Team..." />}
