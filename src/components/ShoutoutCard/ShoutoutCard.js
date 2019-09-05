e63c253830a04be3b284d4e03d3faabeb7aef279
erozonachi <erozonachi.james@gmail.com>
2019-09-05 15:29:25 +0100

    Remove unused placeholder import in shoutout card

--- src/components/ShoutoutCard/ShoutoutCard.js
+++ src/components/ShoutoutCard/ShoutoutCard.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { ShoutoutCardContainer, Emoji, AvatarsContainer, Time, ShoutoutText } from './shoutout.styles';
+import {
+  ShoutoutCardContainer,
+  Emoji,
+  AvatarsContainer,
+  Time,
+  ShoutoutText,
+} from './shoutout.styles';
 import User from '../User';
 import confetti from '../../assets/confetti.png';
-import placeholder from '../../assets/placeholder.png';
 
 const ShoutoutCard = props => {
   const { margin, praiseGiver, giverAvatar, receiverAvatar, praiseTaker, time, praiseText } = props;
 
   return (
-        <ShoutoutCardContainer margin={margin}>
-            <AvatarsContainer>
-                <User img={giverAvatar} name={praiseGiver} />
-                <Emoji src={confetti} />
-                <User img={receiverAvatar} name={praiseTaker} />
-                <Time>{time}</Time>
-            </AvatarsContainer>
-            <ShoutoutText>
-               {praiseText}
-            </ShoutoutText>  
-        </ShoutoutCardContainer>
-    );
+    <ShoutoutCardContainer margin={margin}>
+      <AvatarsContainer>
+        <User img={giverAvatar} name={praiseGiver} />
+        <Emoji src={confetti} />
+        <User img={receiverAvatar} name={praiseTaker} />
+        <Time>{time}</Time>
+      </AvatarsContainer>
+      <ShoutoutText>{praiseText}</ShoutoutText>
+    </ShoutoutCardContainer>
+  );
 };
 
 export default ShoutoutCard;
