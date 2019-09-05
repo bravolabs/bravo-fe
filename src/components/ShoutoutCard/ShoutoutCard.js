bac3f806bc9d98a9c810408d67075a3fb71733f8
erozonachi <erozonachi.james@gmail.com>
2019-09-05 15:41:24 +0100

    Default to placeholder for avatar in shoutout card

--- src/components/ShoutoutCard/ShoutoutCard.js
+++ src/components/ShoutoutCard/ShoutoutCard.js
@@ -8,6 +8,7 @@ import {
 } from './shoutout.styles';
 import User from '../User';
 import confetti from '../../assets/confetti.png';
+import placeholder from '../../assets/placeholder.png';
 
 const ShoutoutCard = props => {
   const { margin, praiseGiver, giverAvatar, receiverAvatar, praiseTaker, time, praiseText } = props;
@@ -15,9 +16,9 @@ const ShoutoutCard = props => {
   return (
     <ShoutoutCardContainer margin={margin}>
       <AvatarsContainer>
-        <User img={giverAvatar} name={praiseGiver} />
+        <User img={giverAvatar || placeholder} name={praiseGiver} />
         <Emoji src={confetti} />
-        <User img={receiverAvatar} name={praiseTaker} />
+        <User img={receiverAvatar || placeholder} name={praiseTaker} />
         <Time>{time}</Time>
       </AvatarsContainer>
       <ShoutoutText>{praiseText}</ShoutoutText>
