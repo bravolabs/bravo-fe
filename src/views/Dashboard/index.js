591fa3c10595f88a409b110209b2144af6c02ce5
erozonachi <erozonachi.james@gmail.com>
2019-09-05 12:45:47 +0100

    Use children props in dashboard component

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import SideNav from '../../components/SideNav';
 
-const Dashboard = () => (
+const Dashboard = ({ children }) => (
   <>
     <SideNav />
+    {children}
   </>
 );
 
