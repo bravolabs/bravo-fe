156a67a32d627620e83ce2dc673be34ecfc7fd16
erozonachi <erozonachi.james@gmail.com>
2019-09-05 14:57:54 +0100

    Remove sidenav from user profile view component

--- src/views/UserProfileView/index.js
+++ src/views/UserProfileView/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import SideNav from '../../components/SideNav';
 import UserProfile from '../../components/UserProfile';
 
 const UserProfileView = props => {
   const { user } = props;
 
-  return (
-      <>
-        <SideNav />
-        <UserProfile user={user} />
-      </>
-    );
+  return <UserProfile user={user} />;
 };
 
 export default connect(
