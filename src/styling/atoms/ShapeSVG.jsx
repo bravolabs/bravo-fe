284406ed8066e8f939dc18126e61d6c63f7b2437
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-06 08:00:20 +0100

    Add ShapeSVG styled component

---
+++ src/styling/atoms/ShapeSVG.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ShapeSVG = styled.svg`
+  ${props => (props.svgHeight ? `height: ${props.svgHeight};` : null)}
+  ${props => (props.svgWidth ? `width: ${props.svgWidth};` : null)}
+    .paths {
+    fill: ${props => (props.color ? props.color : '#ffff')};
+    stroke: ${props => (props.color ? props.color : '#ffff')};
+    stroke-miterlimit: ${props => (props.mitter ? props.mitter : '10')};
+    stroke-width: ${props => (props.strokeWidth ? props.strokeWidth : '0px')};
+  }
+`;
