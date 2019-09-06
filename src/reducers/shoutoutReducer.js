aa763a7fdfe91b9df340918fceb065945c4a691f
erozonachi <erozonachi.james@gmail.com>
2019-09-06 16:08:17 +0100

    Set profile shoutouts to null in other cases

--- src/reducers/shoutoutReducer.js
+++ src/reducers/shoutoutReducer.js
@@ -10,11 +10,29 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.SET_SINGLE_SHOUTOUT:
-      return { ...state, singleShoutout: action.payload, fetching: false, error: null };
+      return {
+        ...state,
+        singleShoutout: action.payload,
+        fetching: false,
+        profileShoutouts: null,
+        error: null,
+      };
     case types.FETCHING_SHOUTOUT:
-      return { ...state, error: null, singleShoutout: null, fetching: true };
+      return {
+        ...state,
+        error: null,
+        singleShoutout: null,
+        profileShoutouts: null,
+        fetching: true,
+      };
     case types.SHOUTOUT_ERROR:
-      return { ...state, error: action.payload, singleShoutout: null, fetching: false };
+      return {
+        ...state,
+        error: action.payload,
+        singleShoutout: null,
+        profileShoutouts: null,
+        fetching: false,
+      };
     default:
       return state;
   }
