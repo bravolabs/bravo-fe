9dd0f674bfcd8611e08d57af00033c3eeb1f5aaf
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-06 08:31:05 +0100

    Format options button svg

--- src/styling/atoms/SVGs/Options.jsx
+++ src/styling/atoms/SVGs/Options.jsx
@@ -5,9 +5,9 @@ export default function Options(props) {
   return (
     <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 5" {...props}>
       <g>
-        <circle cx="2.5" cy="2.5" r="2.5"/>
-        <circle cx="10.5" cy="2.5" r="2.5"/>
-        <circle cx="18.5" cy="2.5" r="2.5"/>
+        <circle cx="2.5" cy="2.5" r="2.5" />
+        <circle cx="10.5" cy="2.5" r="2.5" />
+        <circle cx="18.5" cy="2.5" r="2.5" />
       </g>
     </SVG>
   );
