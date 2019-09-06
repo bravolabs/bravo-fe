a8b59fb0e399cafbd6d9cb8fa5ce53416442c2f3
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-06 08:30:46 +0100

    Add options button svg

---
+++ src/styling/atoms/SVGs/Options.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SVG from '../SVG';
+
+export default function Options(props) {
+  return (
+    <SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 5" {...props}>
+      <g>
+        <circle cx="2.5" cy="2.5" r="2.5"/>
+        <circle cx="10.5" cy="2.5" r="2.5"/>
+        <circle cx="18.5" cy="2.5" r="2.5"/>
+      </g>
+    </SVG>
+  );
+}
