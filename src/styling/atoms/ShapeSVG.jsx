83385b37c22bc488c8e5ef54d05c56b7c5a54ac1
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-06 08:24:20 +0100

    Rewrite props in ShapeSVG to be more reusable

--- src/styling/atoms/ShapeSVG.jsx
+++ src/styling/atoms/ShapeSVG.jsx
@@ -4,10 +4,10 @@ const ShapeSVG = styled.svg`
   ${props => (props.svgHeight ? `height: ${props.svgHeight};` : null)}
   ${props => (props.svgWidth ? `width: ${props.svgWidth};` : null)}
     g {
-    fill: ${props => (props.color ? props.color : '#ffff')};
-    stroke: ${props => (props.color ? props.color : '#ffff')};
-    stroke-miterlimit: ${props => (props.mitter ? props.mitter : '10')};
-    stroke-width: ${props => (props.strokeWidth ? props.strokeWidth : '0px')};
+    fill: ${props => props.fillColor || 'none'};
+    stroke: ${props => props.strokeColor || 'none'};
+    stroke-miterlimit: ${props => props.mitter || '10'};
+    stroke-width: ${props => props.strokeWidth || '0px'};
   }
 `;
 
