bf2d7d1c27d162a6f4f3cd462793988745afc41c
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-06 08:09:03 +0100

    Convert logo to new ShapeSVG component for testing

--- src/styling/atoms/PathSVG.jsx
+++ src/styling/atoms/PathSVG.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const PathSVG = styled.svg`
   ${props => (props.svgHeight ? `height: ${props.svgHeight};` : null)}
   ${props => (props.svgWidth ? `width: ${props.svgWidth};` : null)}
-    .paths {
+    g {
     fill: none;
     stroke: ${props => (props.color ? props.color : '#ffff')};
     stroke-miterlimit: ${props => (props.mitter ? props.mitter : '10')};
--- src/styling/atoms/ShapeSVG.jsx
+++ src/styling/atoms/ShapeSVG.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ShapeSVG = styled.svg`
   ${props => (props.svgHeight ? `height: ${props.svgHeight};` : null)}
   ${props => (props.svgWidth ? `width: ${props.svgWidth};` : null)}
-    .paths {
+    g {
     fill: ${props => (props.color ? props.color : '#ffff')};
     stroke: ${props => (props.color ? props.color : '#ffff')};
     stroke-miterlimit: ${props => (props.mitter ? props.mitter : '10')};
