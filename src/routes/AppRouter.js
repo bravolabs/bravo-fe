b736e6ab7543b4d1f43cee0e4249b79c0b4252d0
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-06 08:52:42 +0100

    Add route to welcome message

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -5,6 +5,7 @@ import { Redirect } from 'react-router-dom';
 
 import Home from '../components/Home';
 import Slack from '../components/Slack';
+import Welcome from '../components/Welcome';
 import ProtectedRoute from '../components/ProtectedRoute';
 
 import Shoutout from '../views/SingleShoutout';
@@ -15,6 +16,7 @@ const AppRouter = () => (
     <Route exact path="/" component={Home} />
     <Route path="/slack" component={Slack} />
     <ProtectedRoute path="/shoutout" component={Shoutout} />
+    <ProtectedRoute path="/welcome" component={Welcome} />
     <Route exact path="/user" component={UserProfileView} />
     <Route render={() => <Redirect to="/" />} />
   </Switch>
