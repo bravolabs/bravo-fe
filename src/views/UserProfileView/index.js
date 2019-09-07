84c1234356336461fff715c1565a736731f4d99b
erozonachi <erozonachi.james@gmail.com>
2019-09-07 18:16:53 +0100

    Import and use effect hook in user profile view

--- src/views/UserProfileView/index.js
+++ src/views/UserProfileView/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getProfileShoutouts } from '../../actions/shoutouts';
 
 import UserProfile from '../../components/UserProfile';
 
-const UserProfileView = ({ user, shoutouts }) => {
+const UserProfileView = ({ user, shoutouts, getProfileShoutouts }) => {
+  useEffect(() => {
+    if (!shoutouts) {
+      getProfileShoutouts();
+    }
+  }, []);
   return <UserProfile user={user} shoutouts={shoutouts} />;
 };
 
