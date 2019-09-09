c7333620d14c769116405a94cf416abfd28d4bd3
erozonachi <erozonachi.james@gmail.com>
2019-09-09 14:00:16 +0100

    Import and use rightbar in dashboard component

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SideNav from '../../components/SideNav';
+import RightBar from '../../components/RightBar';
 import MainContent from '../../components/MainContent';
 
 const Dashboard = ({ children }) => (
   <>
     <SideNav />
     <MainContent>{children}</MainContent>
+    <RightBar />
   </>
 );
 
