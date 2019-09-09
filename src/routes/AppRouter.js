0b90becea83d7da9207cd0b2abc3be498abaf4a2
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-09 15:52:18 +0100

    Fix import

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from '../components/Home';
 import SignInRedirect from '../components/SignInRedirect';
