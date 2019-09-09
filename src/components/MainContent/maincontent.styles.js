8010341fcf29151f233fa819e4be9e890f089db4
erozonachi <erozonachi.james@gmail.com>
2019-09-09 12:26:18 +0100

    Add styles to the main content containee
    Add styles to the main content container styled component.

--- src/components/MainContent/maincontent.styles.js
+++ src/components/MainContent/maincontent.styles.js
@@ -1,3 +1,12 @@
 import styled from 'styled-components';
 
-export const MainContentContainer = styled.div``;
+export const MainContentContainer = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 72px;
+  right: 332px;
+  display: flex;
+  flex-direction: column;
+  max-width: 750px;
+  background: #f9fbfd;
+`;
