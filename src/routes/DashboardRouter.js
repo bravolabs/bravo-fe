c9c110e0063f35e91eb0192e21514d137709ff59
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-09 16:04:54 +0100

    Render Routes inside dashboard view

--- src/routes/DashboardRouter.js
+++ src/routes/DashboardRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
 
-import SideNav from '../components/SideNav';
+import Dashboard from '../views/Dashboard';
 
 import Slack from '../components/Slack';
 import Welcome from '../components/Welcome';
@@ -14,13 +14,14 @@ import Team from '../components/Team';
 
 const DashboardRouter = () => (
   <Switch>
-    <SideNav />
-    <Route path="/slack" component={Slack} />
-    <ProtectedRoute path="/welcome" component={Welcome} />
-    <ProtectedRoute path="/shoutout/:id" component={Shoutout} />
-    <ProtectedRoute path="/team" component={Team} />
-    <ProtectedRoute path="/profile" component={UserProfileView} />
-    <Route render={() => <Redirect to="/" />} />
+    <Dashboard>
+      <Route path="/slack" component={Slack} />
+      <ProtectedRoute path="/welcome" component={Welcome} />
+      <ProtectedRoute path="/shoutout/:id" component={Shoutout} />
+      <ProtectedRoute path="/team" component={Team} />
+      <ProtectedRoute path="/profile" component={UserProfileView} />
+      <Route render={() => <Redirect to="/" />} />
+    </Dashboard>
   </Switch>
 );
 
