a2af7b96bf1ff9aa26d44a6fdb5fcbb809be28f3
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-09 09:06:21 +0100

    Switch on face to get border color

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -3,6 +3,21 @@ import { TooltipWrapper, Tooltip as TooltipContainer } from '../../styling/atoms
 
 const Tooltip = props => {
   const { face, position, color, text } = props;
+
+  const borderColor = (() => {
+    switch (face) {
+      case 'left':
+        return `transparent ${color} transparent transparent`;
+      case 'right':
+        return `transparent transparent transparent ${color}`;
+      case 'up':
+        return `transparent transparent ${color} transparent`;
+      case 'down':
+        return `${color} transparent transparent transparent`;
+      default:
+        return `${color} transparent transparent transparent`;
+    }
+  })();
 };
 
 export default Tooltip;
