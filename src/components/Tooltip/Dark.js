83d112d03a016d86ce5b2c0c169c4810f0e0c369
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-09 10:30:45 +0100

    Create and export dark tooltio

--- src/components/Tooltip/Dark.js
+++ src/components/Tooltip/Dark.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import Tooltip from './index';
+import FontColors from '../../styling/variables/FontColors';
+import UIColors from '../../styling/variables/UIColors';
+
+const TooltipDark = props => {
+  return <Tooltip bgColor={FontColors.Dark} color={UIColors.white} {...props} />;
+};
+
+export default TooltipDark;
