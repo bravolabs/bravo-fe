b5c444c1e667527139337df3fe50c882923b1218
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-09 16:01:11 +0100

    Add content container

--- src/routes/DashboardRouter.js
+++ src/routes/DashboardRouter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
-import styled from 'styled-components';
 
 import SideNav from '../components/SideNav';
 
--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import SideNav from '../../components/SideNav';
 
+const ContentContainer = styled.section`
+  margin-left: 120px;
+  flex-grow: 1;
+`;
 const Dashboard = ({ children }) => (
   <>
     <SideNav />
