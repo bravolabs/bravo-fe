c7d6121e56522921126f3313605c68e56cf3678b
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-09 08:59:04 +0100

    Define props for tooltip

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { TooltipWrapper, Tooltip as TooltipContainer } from '../../styling/atoms/Tooltip';
 
-const Tooltip = props => {};
+const Tooltip = props => {
+  const { face, position, color, text } = props;
+};
 
 export default Tooltip;
