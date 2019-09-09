1a909bc73a88ebd3c7e611bf33552efc135827ba
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-09 15:22:48 +0100

    Add OverflowMenu componenet

--- src/components/OverflowMenu/index.js
+++ src/components/OverflowMenu/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import { MenuContainer, MenuItem } from '../../styling/atoms/OverflowMenu';
+import UIColors from '../../styling/variables/UIColors';
+import FontColors from '../../styling/variables/FontColors';
+import ShapeStyles from '../../styling/variables/ShapeStyles';
 
-export const OverflowMenu = props => {};
+export const OverflowMenu = props => {
+  const { x, y } 
+  return (
+    <MenuContainer shadow={ShapeStyles.elevation} x={x} y={y} >
+      { props.children }
+    </MenuContainer>
+  );
+};
