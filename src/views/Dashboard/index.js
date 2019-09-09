9e1dc9748e8592ed656cc506ede2e9ed3d94386a
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-09 16:02:59 +0100

    Add PageContainer

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -7,6 +7,16 @@ const ContentContainer = styled.section`
   margin-left: 120px;
   flex-grow: 1;
 `;
+
+const PageContainer = styled.section`
+  display: flex;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+`;
+
 const Dashboard = ({ children }) => (
   <>
     <SideNav />
