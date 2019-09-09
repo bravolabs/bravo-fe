ba02eed40718123b4f95ef0d5b4d2d82a764fdce
erozonachi <erozonachi.james@gmail.com>
2019-09-09 11:40:44 +0100

    Add styles to rightbar container styled component

--- src/components/RightBar/rightbar.styles.js
+++ src/components/RightBar/rightbar.styles.js
@@ -1,3 +1,15 @@
 import styled from 'styled-components';
 
-export const RightBarContainer = styled.div``;
+export const RightBarContainer = styled.div`
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 332px;
+  height: 100vh;
+  padding: 140px 30px 0px 30px;
+  background: #f9fbfd;
+`;
