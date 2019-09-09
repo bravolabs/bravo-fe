f0494d5c810fb045b04449db2cb1f8a3d929b862
erozonachi <erozonachi.james@gmail.com>
2019-09-09 12:31:48 +0100

    Import and use miancontentcontainer

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import { MainContentContainer } from './maincontent.styles';
 
-const MainContent = ({ children }) => <></>;
+const MainContent = ({ children }) => <MainContentContainer>{children}</MainContentContainer>;
 
 export default MainContent;
