ae5841377185af262f38d9d4dd699fcf6cfaf9f9
erozonachi <erozonachi.james@gmail.com>
2019-09-09 14:47:36 +0100

    Change shoutout text width value to 100%

--- src/components/ShoutoutCard/shoutout.styles.js
+++ src/components/ShoutoutCard/shoutout.styles.js
@@ -40,7 +40,7 @@ export const Time = styled.div`
 
 export const ShoutoutText = styled(BodyText)`
   margin-top: 1.5rem;
-  width: 800px;
+  width: 100%;
   height: 8rem;
   line-height: 2.2rem;
 `;
