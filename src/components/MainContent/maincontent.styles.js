735c260d647297cdda5883df9d69832842e3dfa9
erozonachi <erozonachi.james@gmail.com>
2019-09-10 10:16:32 +0100

    Change maincontent left value to 90px

--- src/components/MainContent/maincontent.styles.js
+++ src/components/MainContent/maincontent.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const MainContentContainer = styled.div`
   position: relative;
   top: 0px;
-  left: 80px;
+  left: 90px;
   right: 374px;
   display: flex;
   flex-direction: column;
