3114ccd3b27bd3464440e8ce5d5304b5c9ae87c3
erozonachi <erozonachi.james@gmail.com>
2019-09-10 20:57:47 +0100

    Import and use link in member card component

--- src/components/Cards/MemberCard.js
+++ src/components/Cards/MemberCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { MemberCardContainer, MemberCardAction } from './cards.styles';
 import User from '../User';
 import placeholder from '../../assets/placeholder.png';
@@ -6,7 +7,9 @@ import placeholder from '../../assets/placeholder.png';
 const MemberCard = ({ id, name, avatar }) => (
   <MemberCardContainer>
     <User img={avatar || placeholder} alt={name} name={name} />
-    <MemberCardAction>View Profile</MemberCardAction>
+    <MemberCardAction>
+      <Link to={`/members/${id}`}>View Profile</Link>
+    </MemberCardAction>
   </MemberCardContainer>
 );
 
