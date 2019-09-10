23eec6cafd10a287ebee583251bf0aa5ede1adbb
erozonachi <erozonachi.james@gmail.com>
2019-09-10 15:57:49 +0100

    Remove user id prop from member card component

--- src/components/Cards/MemberCards.js
+++ src/components/Cards/MemberCards.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MemberCardsContainer } from './cards.styles';
 import MemberCard from './MemberCard';
 
-const MemberCards = ({ members, userId }) => (
+const MemberCards = ({ members }) => (
   <MemberCardsContainer>
     {members.map(member => (
       <MemberCard {...member} key={member.id} />
