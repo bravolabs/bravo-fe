d33ceab6055bd2a2582e6dfc5dcab3ee828be0af
erozonachi <erozonachi.james@gmail.com>
2019-09-10 15:13:19 +0100

    Remove unused handle-action-click function

--- src/components/Cards/MemberCard.js
+++ src/components/Cards/MemberCard.js
@@ -2,15 +2,6 @@ import React from 'react';
 import { MemberCardContainer, MemberCardAction, ViewProfile } from './cards.styles';
 import User from '../User';
 import placeholder from '../../assets/placeholder.png';
-const handleActionClick = id => {
-  const target = document.querySelector(`#${id}`);
-  const val = target.style.display;
-  if (val === 'none' || val === '') {
-    target.style.display = 'block';
-  } else {
-    target.style.display = 'none';
-  }
-};
 
 const MemberCard = ({ id, name, avatar }) => (
   <MemberCardContainer>
