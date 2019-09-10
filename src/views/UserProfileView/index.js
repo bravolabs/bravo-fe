d41ba7964fec83788a9ef82f82f653839d30e516
erozonachi <erozonachi.james@gmail.com>
2019-09-10 17:48:12 +0100

    Pass member from state to userprofileview as prop

--- src/views/UserProfileView/index.js
+++ src/views/UserProfileView/index.js
@@ -17,6 +17,7 @@ export default connect(
   state => ({
     user: state.slack.user,
     shoutouts: state.shoutouts.profileShoutouts,
+    members: state.team.members,
   }),
   { getProfileShoutouts }
 )(UserProfileView);
