fe84435beda7b203eca0464280dbe0d4002ffeea
erozonachi <erozonachi.james@gmail.com>
2019-09-10 15:55:21 +0100

    Add user id prop to the member cards component

--- src/components/Cards/MemberCards.js
+++ src/components/Cards/MemberCards.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MemberCardsContainer } from './cards.styles';
 import MemberCard from './MemberCard';
 
-const MemberCards = ({ members }) => (
+const MemberCards = ({ members, userId }) => (
   <MemberCardsContainer>
     {members.map(member => (
       <MemberCard {...member} key={member.id} />
