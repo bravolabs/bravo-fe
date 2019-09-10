9719fd1ff0e7a325f8074164252cdc168ff8d86b
erozonachi <erozonachi.james@gmail.com>
2019-09-10 11:31:52 +0100

    Remove authorization from axios header

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,6 @@ export const Axios = (token = null) => {
   return axios.create({
     headers: {
       'Content-Type': 'application/json',
-      Authorization: token,
     },
     baseURL: process.env.REACT_APP_API_HOST,
   });
