9002b33cc244dccffac7fb773c34804386a1d411
Noble Obioma <nobioma1@gmail.com>
2019-09-10 15:07:52 +0100

    Create RightBar component with placeholder content for modal

--- src/components/RightBar/index.js
+++ src/components/RightBar/index.js
@@ -1,6 +1,54 @@
 import React from 'react';
-import { RightBarContainer } from './rightbar.styles';
+import { RightBarContainer, RightBarCard, TextContent, MediumCard } from './rightbar.styles';
+import getStarted from '../../assets/get-started.svg';
+import ShapeStyles from '../../styling/variables/ShapeStyles';
+import { BodyText } from '../../styling/atoms/Fonts';
+import MiniRightCard from './MiniRightCard';
+import group from '../../assets/group.svg';
+import shoutout from '../../assets/shoutout.svg';
+import howto from '../../assets/howto.svg';
 
-const RightBar = () => <RightBarContainer></RightBarContainer>;
+const description = {
+  whatIsBravo: {
+    image: group,
+    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni omnis delectus
+    nemo, maxime molestiae dolorem numquam mollitia, voluptate ea, accusamus excepturi
+    deleniti ratione sapiente! Laudantium, aperiam doloribus. Odit, aut.`,
+  },
+  whatAreShoutouts: {
+    image: shoutout,
+    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni omnis delectus
+    nemo, maxime molestiae dolorem numquam mollitia, voluptate ea, accusamus excepturi
+    deleniti ratione sapiente! Laudantium, aperiam doloribus. Odit, aut.`,
+  },
+  howToGetBravos: {
+    image: howto,
+    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni omnis delectus
+    nemo, maxime molestiae dolorem numquam mollitia, voluptate ea, accusamus excepturi
+    deleniti ratione sapiente! Laudantium, aperiam doloribus. Odit, aut.`,
+  },
+};
+
+const RightBar = () => (
+  <RightBarContainer>
+    <RightBarCard shadow={ShapeStyles.elevation.elevate}>
+      <header>
+        <img src={getStarted} alt="get started" />
+      </header>
+      <TextContent>
+        <p>1. Connect Slack</p>
+        <p>2. Receive Shoutout</p>
+        <p>3. View Shoutout</p>
+      </TextContent>
+    </RightBarCard>
+
+    <section>
+      <BodyText>Learn about Bravo</BodyText>
+      <MiniRightCard text="What is Bravo?" description={description.whatIsBravo} />
+      <MiniRightCard text="What are shoutouts?" description={description.whatAreShoutouts} />
+      <MiniRightCard text="How do I get more Bravos?" description={description.howToGetBravos} />
+    </section>
+  </RightBarContainer>
+);
 
 export default RightBar;
--- src/components/RightBar/rightbar.styles.js
+++ src/components/RightBar/rightbar.styles.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Card } from '../../styling/atoms/Card';
+import colors from '../../styling/variables/UIColors';
 
 export const RightBarContainer = styled.div`
   position: fixed;
@@ -6,11 +8,62 @@ export const RightBarContainer = styled.div`
   right: 0px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: center;
   width: 374px;
   height: 100vh;
   padding: 140px 30px 0px 30px;
   background: #f9fbfd;
-  border: 1px solid rgba(57, 91, 234, 0.9);
+
+  section {
+    display: flex;
+    flex-direction: column;
+  }
+
+  @media (max-width: 700px) {
+    display: none;
+  }
+`;
+
+export const RightBarCard = styled(Card)`
+  padding: 0px;
+  width: 292px;
+  min-height: 50px;
+  margin-bottom: 50px;
+
+  header {
+    background-color: ${colors.brand};
+    border-radius: 8px 8px 0 0;
+
+    img {
+      border-radius: 8px 8px 0 0;
+    }
+  }
+`;
+
+export const TextContent = styled.div`
+  height: 100px;
+  padding: 10px;
+
+  p {
+    line-height: 1.8;
+  }
+`;
+
+export const MediumCard = styled(Card)`
+  width: 292px;
+  height: 50px;
+  display: flex;
+  padding: 10px;
+  align-items: center;
+  margin: 10px 0;
+  transition: 0.3s ease-in-out;
+
+  input {
+    margin-right: 9px;
+  }
+
+  &:hover {
+    cursor: pointer;
+    background-color: lightgray;
+  }
 `;
