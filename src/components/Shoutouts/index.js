33adcaf0671d48d49182a871f754d52983392ab7
erozonachi <erozonachi.james@gmail.com>
2019-09-11 14:50:32 +0100

    Import and use card container in shoutouts component

--- src/components/Shoutouts/index.js
+++ src/components/Shoutouts/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ShoutoutsContainer, Title } from './shoutouts.styles';
+import { ShoutoutsContainer, Title, CardContainer } from './shoutouts.styles';
 
 const Shoutouts = ({ ...props }) => (
   <ShoutoutsContainer>
     <Title>Shoutouts</Title>
+    <CardContainer></CardContainer>
   </ShoutoutsContainer>
 );
