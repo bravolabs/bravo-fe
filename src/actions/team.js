083c8bd92ee8b5a282d7139540f8dd1f9babfda8
erozonachi <erozonachi.james@gmail.com>
2019-09-11 12:01:00 +0100

    Remove organization parameter from fetchteaminfo()

--- src/actions/team.js
+++ src/actions/team.js
@@ -1,13 +1,16 @@
 import { axiosWithAuth } from '../utils/axios';
+import localstorage from '../utils/localstorage';
 
 export const FETCHING_TEAM = 'FETCHING_TEAM';
 export const TEAM_FETCH_SUCCESS = 'TEAM_FETCH_SUCCESS';
 export const TEAM_FETCH_FAIL = 'TEAM_FETCH_FAIL';
 
-export const fetchTeamInfo = organization => async dispatch => {
+export const fetchTeamInfo = () => async dispatch => {
   dispatch({ type: FETCHING_TEAM });
   try {
-    const { data } = await axiosWithAuth().get(`/api/organizations/${organization}/users`);
+    const { data } = await axiosWithAuth().get(
+      `/api/organizations/${localstorage.get().org_id}/users`
+    );
 
     dispatch({ type: TEAM_FETCH_SUCCESS, payload: data.data || data });
   } catch (error) {
