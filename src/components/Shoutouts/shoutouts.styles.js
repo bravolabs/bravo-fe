f1b4bfd09d13dc1d5c09a3ed22565537c1879d20
erozonachi <erozonachi.james@gmail.com>
2019-09-11 14:22:57 +0100

    Add styles to shoutouts container component

--- src/components/Shoutouts/shoutouts.styles.js
+++ src/components/Shoutouts/shoutouts.styles.js
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const ShoutoutsContainer = styled.div``;
+export const ShoutoutsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 900px;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  margin-top: 6rem;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+`;
