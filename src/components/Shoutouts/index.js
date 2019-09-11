71a566474cf5415a66259f29ec3548329d5fa9ef
erozonachi <erozonachi.james@gmail.com>
2019-09-11 14:39:55 +0100

    Import and use shoutouts-container in shoutouts

--- src/components/Shoutouts/index.js
+++ src/components/Shoutouts/index.js
@@ -1,3 +1,4 @@
 import React from 'react';
+import { ShoutoutsContainer } from './shoutouts.styles';
 
-const Shoutouts = ({ ...props }) => <></>;
+const Shoutouts = ({ ...props }) => <ShoutoutsContainer></ShoutoutsContainer>;
