0ec76a2e24272170516108c3d65d366e6a072388
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-11 12:00:45 +0100

    Chore: Flesh out the leaderboard action creators

--- src/actions/leaderboard.js
+++ src/actions/leaderboard.js
@@ -8,7 +8,8 @@ export const LEADERBOARD_FETCH_FAIL = 'LEADERBOARD_FETCH_FAIL';
 export const fetchLeaderBoard = () => async dispatch => {
   dispatch({ type: FETCHING_LEADERBOARD });
   try {
-
+    const { data } = await axiosWithAuth().get('/api/leaderboard');
+    dispatch({ type: LEADERBOARD_FETCH_SUCCESS, payload: data.data || data });
   } catch (error) {
     if (error.response) {
       dispatch({ tyoe: LEADERBOARD_FETCH_FAIL, message: error.response.data.message });
