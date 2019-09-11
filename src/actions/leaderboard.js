82805e6fade22aefd52fa99b7467af52a816874a
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-11 11:15:15 +0100

    Chore: Create the action types for dispatching actions needed for successful operation of the leaderboard page

---
+++ src/actions/leaderboard.js
@@ -0,0 +1,5 @@
+import { axiosWithAuth } from '../utils/axios';
+
+export const FETCHING_LEADERBOARD = 'FETCHING_LEADERBOARD';
+export const LEADERBOARD_FETCH_SUCCESS = 'LEADERBOARD_FETCH_SUCCESS';
+export const LEADERBOARD_FETCH_FAIL = 'LEADERBOARD_FETCH_FAIL';
