bd1d584c88207c5741d199503210d5db26d606ab
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-11 11:31:17 +0100

    CHore: dispatch first fetch action, when we are trying to load or ftech leaderboard

--- src/actions/leaderboard.js
+++ src/actions/leaderboard.js
@@ -3,3 +3,7 @@ import { axiosWithAuth } from '../utils/axios';
 export const FETCHING_LEADERBOARD = 'FETCHING_LEADERBOARD';
 export const LEADERBOARD_FETCH_SUCCESS = 'LEADERBOARD_FETCH_SUCCESS';
 export const LEADERBOARD_FETCH_FAIL = 'LEADERBOARD_FETCH_FAIL';
+
+export const fetchLeaderBoard = () => async dispatch => {
+  dispatch({ type: FETCHING_LEADERBOARD });
+};
