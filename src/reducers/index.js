d41a2a9ff18aa321152bcb62c44670ef962aaa2d
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-11 13:48:40 +0100

    Feat: Add leaderboard reducer to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import slack from './slackReducer';
 import team from './teamReducer';
 import shoutouts from './shoutoutReducer';
 import users from './usersReducer';
+import leaderboard from './leaderboardReducer';
 
 const rootReducer = combineReducers({
   slack,
   team,
   shoutouts,
   users,
+  leaderboard,
 });
 
 export default rootReducer;
