c3ea008740cf7198a1676f4e61ce8be42fdf9178
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-11 14:24:30 +0100

    Chore: Flesh out the leaderboard view

--- src/components/LeaderBoard/index.js
+++ src/components/LeaderBoard/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+// import {  } from './leaderboard.styles';
+import LeaderboardCards from '../Cards/LeaderboardCards';
+import { fetchLeaderBoard } from '../../actions/leaderboard';
 
-export default function index() {
+import Loader from '../Loader';
+import DisplayCard from '../Cards/DisplayCard';
+import bravoParty from '../../assets/bravo-party.svg';
+import localstorage from '../../utils/localstorage';
+
+const LeaderBoard = ({ leaderboard, fetchLeaderBoard }) => {
+  useEffect(() => {
+    if (leaderboard && !leaderboard.leaderboard) {
+      fetchLeaderBoard();
+    }
+  }, []);
   return (
     <div>
       <h1>Hello LeaderBoard</h1>
     </div>
   );
-}
+};
+
+const mapStateToProps = state => {
+  return {
+    leaderboard: state.leaderboard,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchLeaderBoard }
+)(LeaderBoard);
