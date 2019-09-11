3ab69a0f8369213124d9cef57c5863326ef4491d
Noble Obioma <nobioma1@gmail.com>
2019-09-11 07:39:32 +0100

    Reduce modal height and width

--- src/components/RightBar/modal.styles.js
+++ src/components/RightBar/modal.styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const ModalContainer = styled.div`
-  width: 550px;
-  height: 485px;
+  width: 500px;
+  height: 450px;
   display: flex;
   flex-direction: column;
 `;
@@ -14,7 +14,7 @@ export const Imagecontainer = styled.div`
   align-items: center;
 
   img {
-    width: 300px;
+    width: 250px;
   }
 `;
 
