4b44708d4a0d67cf3f18e89ae63ff32cb7dbc1be
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-11 11:53:05 +0100

    Chore: Handle error as a result of asynchronous operations in leaderboard action creators catch block

--- src/actions/leaderboard.js
+++ src/actions/leaderboard.js
@@ -1,4 +1,5 @@
 import { axiosWithAuth } from '../utils/axios';
+import { TEAM_FETCH_FAIL } from './team';
 
 export const FETCHING_LEADERBOARD = 'FETCHING_LEADERBOARD';
 export const LEADERBOARD_FETCH_SUCCESS = 'LEADERBOARD_FETCH_SUCCESS';
@@ -6,4 +7,13 @@ export const LEADERBOARD_FETCH_FAIL = 'LEADERBOARD_FETCH_FAIL';
 
 export const fetchLeaderBoard = () => async dispatch => {
   dispatch({ type: FETCHING_LEADERBOARD });
+  try {
+
+  } catch (error) {
+    if (error.response) {
+      dispatch({ tyoe: LEADERBOARD_FETCH_FAIL, message: error.response.data.message });
+      return;
+    }
+    dispatch({ type: TEAM_FETCH_FAIL, message: error.message });
+  } 
 };
