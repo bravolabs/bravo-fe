566f60aa46eb7005f9e5a0a1b0c75d39ac4e9711
erozonachi <erozonachi.james@gmail.com>
2019-09-12 16:09:02 +0100

    Import and use section container in comment section

--- src/components/CommentSection/index.js
+++ src/components/CommentSection/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import { SectionContainer } from './commentsection.styles';
 
-const CommentSection = ({ comments }) => <></>;
+const CommentSection = ({ comments }) => <SectionContainer></SectionContainer>;
 
 export default CommentSection;
