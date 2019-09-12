1d7a5b0840d944458f945f5a648e597a120a2cc1
erozonachi <erozonachi.james@gmail.com>
2019-09-13 00:51:12 +0100

    Fix 0 rendering bug

--- src/views/SingleShoutout/index.js
+++ src/views/SingleShoutout/index.js
@@ -43,7 +43,7 @@ const View = ({ shoutout, comments, getSingleShoutout, getComments, match, fetch
         )}
         {error && <DisplayCard header={<img src={bravoParty} alt="bravo party" />} text={error} />}
       </ShoutoutsContainer>
-      {comments && comments.length && <CommentSection comments={comments} />}
+      {comments && comments.length > 0 && <CommentSection comments={comments} />}
     </>
   );
 };
