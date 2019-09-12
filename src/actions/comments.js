66334eabe04527e17e7b81c622da51fc3c956112
erozonachi <erozonachi.james@gmail.com>
2019-09-12 20:09:17 +0100

    Add and export get comments action dispatcher

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,5 +1,22 @@
+import { axiosWithAuth } from '../utils/axios';
+
 export const types = {
   SET_COMMENTS: 'SET_COMMENTS',
   FETCHING_COMMENTS: 'FETCHING_COMMENTS',
   COMMENTS_ERROR: 'COMMENTS_ERROR',
+};
+
+export const getComments = id => async dispatch => {
+  dispatch({ type: types.FETCHING_COMMENTS });
+  try {
+    const { data } = await axiosWithAuth().get(`/api/shoutouts/${id}/replies`);
+
+    dispatch({ type: types.SET_COMMENTS, payload: data.data || data });
+  } catch (error) {
+    if (error.response) {
+      dispatch({ type: types.COMMENTS_ERROR, payload: error.response.data.message });
+      return;
+    }
+    dispatch({ type: types.COMMENTS_ERROR, payload: error.message });
+  }
 };
