4b135f70bcf72a231f42625784310d8194eb2332
erozonachi <erozonachi.james@gmail.com>
2019-09-12 12:35:41 +0100

    Import and use user component in comment card

--- src/components/Cards/CommentCard.js
+++ src/components/Cards/CommentCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { CommentCardContainer, CommentHeader } from './cards.styles';
+import User from '../User';
 
 const CommentCard = ({ name, avatar, text, timestamp }) => (
   <CommentCardContainer>
-    <CommentHeader></CommentHeader>
+    <CommentHeader>
+      <User img={avatar} name={name} />
+    </CommentHeader>
   </CommentCardContainer>
 );
 
