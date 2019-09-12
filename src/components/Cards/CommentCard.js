bf0a355b74fd6614dd69e27ac553c3be5cfe42f7
erozonachi <erozonachi.james@gmail.com>
2019-09-12 12:19:08 +0100

    Import and use comment card container

--- src/components/Cards/CommentCard.js
+++ src/components/Cards/CommentCard.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { CommentCardContainer } from './cards.styles';
 
-const CommentCard = ({ name, avatar, text, timestamp }) => <></>;
+const CommentCard = ({ name, avatar, text, timestamp }) => (
+  <CommentCardContainer></CommentCardContainer>
+);
 
 export default CommentCard;
