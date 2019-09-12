750ee4fd3ed05841ce6dba0dd80d1df196ceec75
erozonachi <erozonachi.james@gmail.com>
2019-09-12 20:29:16 +0100

    Add comments reducer to the combined reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import slack from './slackReducer';
 import team from './teamReducer';
 import shoutouts from './shoutoutReducer';
 import users from './usersReducer';
+import comments from './commentsReducer';
 
 const rootReducer = combineReducers({
   slack,
   team,
   shoutouts,
   users,
+  comments,
 });
 
 export default rootReducer;
