973577a25ecb91a28773cb9dbbfc308df15629a4
erozonachi <erozonachi.james@gmail.com>
2019-09-13 00:26:08 +0100

    Fix comment timestamp bug

--- src/components/CommentSection/index.js
+++ src/components/CommentSection/index.js
@@ -2,20 +2,28 @@ import React from 'react';
 import { SectionContainer, Title } from './commentsection.styles';
 import CommentCard from '../Cards/CommentCard';
 
-const CommentSection = ({ comments }) => (
-  <SectionContainer>
-    <Title>Comments</Title>
-    {comments &&
-      comments.map(comment => (
-        <CommentCard
-          name={comment.name}
-          avatar={comment.avatar}
-          text={comment.text}
-          timestamp={comment.timestamp}
-          key={comment.id}
-        />
-      ))}
-  </SectionContainer>
-);
+const CommentSection = ({ comments }) => {
+  return (
+    <SectionContainer>
+      <Title>Comments</Title>
+      {comments &&
+        comments.map(comment => (
+          <CommentCard
+            name={comment.name}
+            avatar={comment.avatar}
+            text={comment.text}
+            timestamp={Number(
+              comment.timestamp
+                .replace('.', '')
+                .split('')
+                .splice(0, 13)
+                .join('')
+            )}
+            key={comment.id}
+          />
+        ))}
+    </SectionContainer>
+  );
+};
 
 export default CommentSection;
