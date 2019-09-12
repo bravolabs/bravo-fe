dd1d23e53cef5c23755ca7ed41953483383a5538
Noble Obioma <nobioma1@gmail.com>
2019-09-12 13:25:43 +0100

    styles from home and homenav

--- src/components/Home/home.styles.js
+++ src/components/Home/home.styles.js
@@ -3,16 +3,12 @@ import { PageTitle, BodyText } from '../../styling/atoms/Fonts';
 
 export const Container = styled.div`
   margin: auto;
-  font-family: 'Lato', sans-serif;
-  font-size: 62.5%;
-  max-width: 1440px;
-  max-height: 1000px;
+  max-width: 850px;
 `;
 
 export const Main = styled.div`
-  width: 900px;
-  height: 450px;
-  margin: 60px auto;
+  width: 100%;
+  margin: 150px auto;
   justify-content: space-between;
 `;
 
@@ -20,8 +16,8 @@ export const Intro = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 200px;
-  margin-bottom: 100px;
+  height: 250px;
+  margin-bottom: 50px;
 `;
 
 export const Caption = styled.span`
@@ -33,7 +29,6 @@ export const Caption = styled.span`
 
 export const IntroText = styled.span`
   text-align: center;
-  width: 530px;
   font-size: 20px;
   line-height: 24px;
   font-weight: 300;
@@ -42,11 +37,11 @@ export const IntroText = styled.span`
 
 export const StyledPageTitle = styled(PageTitle)`
   margin: 0 auto;
+  text-align: center;
 `;
 
 export const StyledExtraInfo = styled(BodyText)`
   text-align: center;
   margin: 20px auto 30px auto;
-  width: 530px;
   height: 45px;
 `;
---
+++ src/components/Home/homeNav.styles.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import colors from '../../styling/variables/UIColors';
+
+export const HomeNavContainer = styled.nav`
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+  background-color: ${colors.light};
+`;
+
+export const NavContainer = styled.nav`
+  width: 980px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+`;
+
+export const Logo = styled.img`
+  width: 48px;
+  height: 48px;
+`;
+
+export const NavText = styled.span`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 38px;
+  margin-left: 8px;
+
+  @media (max-width: 350px) {
+    display: none;
+  }
+`;
