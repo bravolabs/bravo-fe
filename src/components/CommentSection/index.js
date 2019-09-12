be1f5d7d8f798489e4f6b2a96561d9a9cfdcc43b
erozonachi <erozonachi.james@gmail.com>
2019-09-12 16:19:43 +0100

    Import and use comment card in comment section

--- src/components/CommentSection/index.js
+++ src/components/CommentSection/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { SectionContainer } from './commentsection.styles';
+import CommentCard from '../Cards/CommentCard';
 
-const CommentSection = ({ comments }) => <SectionContainer></SectionContainer>;
+const CommentSection = ({ comments }) => (
+  <SectionContainer>
+    <CommentCard
+      name="James Erozonachi"
+      avatar={null}
+      text="Yoo Mehh... Killed it!"
+      timestamp="1568301444428"
+    />
+    <CommentCard
+      name="Code Maker"
+      avatar={null}
+      text="Well deserved!.."
+      timestamp="1568301444428"
+    />
+  </SectionContainer>
+);
 
 export default CommentSection;
