497bc5c79a64d1829c21b69e7045c57122e92821
erozonachi <erozonachi.james@gmail.com>
2019-09-12 12:57:03 +0100

    Add styles to the section container component

--- src/components/CommentSection/commentsection.styles.js
+++ src/components/CommentSection/commentsection.styles.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components';
 
-export const SectionContainer = styled.div``;
+export const SectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 900px;
+  width: 100%;
+  padding: 0;
+  padding-left: 4rem;
+  margin: 0;
+`;
