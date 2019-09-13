e6488b7abde4704401ca9cdc7797366c0d8b04e9
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 02:55:58 +0100

    Add components to HamburgerButton

--- src/components/HamburgerButton/index.js
+++ src/components/HamburgerButton/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { ButtonContainer, Slice } from './hamburger.styles';
 
 const HamburgerButton = props => {
-
-}
+  return (
+    <ButtonContainer>
+      <Slice />
+      <Slice />
+      <Slice />
+    </ButtonContainer>
+  );
+};
 
 export default HamburgerButton;
