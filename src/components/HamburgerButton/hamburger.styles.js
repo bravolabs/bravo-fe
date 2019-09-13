d164f1c8853e5f8b7478f4ae690168896b7693a2
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 02:51:27 +0100

    Add basic styles to button container

--- src/components/HamburgerButton/hamburger.styles.js
+++ src/components/HamburgerButton/hamburger.styles.js
@@ -1,4 +1,13 @@
 import styled from 'styled-components';
 import UIColors from '../../styling/variables/UIColors';
 
-export const ButtonContainer = styled.button``;
+export const ButtonContainer = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border: none;
+  background-color: none;
+  width: 2.4rem;
+  height: 1.6rem;
+`;
