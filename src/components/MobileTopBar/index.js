f68928a1cd2ea01514d71bf8dbda2f2f67d0dfe5
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 02:57:08 +0100

    Import and use in top bar

--- src/components/MobileTopBar/index.js
+++ src/components/MobileTopBar/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from '../../styling/atoms/SVGs/Logo';
 import UIColors from '../../styling/variables/UIColors';
+import HamburgerButton from '../HamburgerButton';
 import { TopBarContainer } from './topbar.styles';
 
 const TopBar = props => {
   return (
     <TopBarContainer>
+      <HamburgerButton />
       <Logo fillColor={UIColors.light} svgWidth="3.2rem" svgHeigth="3.2rem" />
     </TopBarContainer>
   );
