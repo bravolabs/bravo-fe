86fa30130c8aa43747510385883fd0b514c16351
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 15:36:39 +0100

    Remove Avatar from mobile avatar

--- src/components/SideNav/MobileAvatar.js
+++ src/components/SideNav/MobileAvatar.js
@@ -2,14 +2,11 @@ import React from 'react';
 import Link from '../UnstyledLink';
 import { ExtraInfo, ComponentTitle } from '../../styling/atoms/Fonts';
 import { MobileAvatarContainer } from './sidenav.styles';
-import Avatar from '../../styling/atoms/SVGs/Avatars';
 
 const MobileAvatar = ({ src, alt, name, link }) => {
   return (
     <Link to={link}>
       <MobileAvatarContainer>
-        <Avatar svgWidth='96px' svgHeight='96px' Color='#5DA3A3' />
-        <Avatar svgWidth='96px' svgHeight='96px' Color='#FF7964' />
         <img src={src} alt={alt} />
         <ComponentTitle>{name}</ComponentTitle>
         <ExtraInfo>View Profile</ExtraInfo>
