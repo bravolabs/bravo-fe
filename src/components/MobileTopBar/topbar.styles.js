531e2516902d22a3819039aa5072a60e58d293c8
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 02:16:57 +0100

    Add TopBarContainer component

--- src/components/MobileTopBar/topbar.styles.js
+++ src/components/MobileTopBar/topbar.styles.js
@@ -1,3 +1,5 @@
 import styled from 'styled-components';
 import UIColors from '../../styling/variables/UIColors';
 import Breakpoints from '../../styling/variables/Breakpoints';
+
+export const TopBarContainer = styled.nav``;
