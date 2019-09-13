457d8055da1a539cef0bc6456c8e4120bbc5fed8
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 07:41:41 +0100

    Add toggle side menu type and action creator

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -1,1 +1,9 @@
-export const types = {};
+export const types = {
+  UI_SIDEMENU_TOGGLE: 'UI_SIDEMENU_TOGGLE',
+};
+
+export const toggleSideMenu = () => {
+  return {
+    type: types.UI_SIDEMENU_TOGGLE,
+  };
+};
