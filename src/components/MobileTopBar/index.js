62aa310db824b3f8c39ab0caa0226a1806fe76f0
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 07:52:11 +0100

    Link topbar to ui state

--- src/components/MobileTopBar/index.js
+++ src/components/MobileTopBar/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Logo from '../../styling/atoms/SVGs/Logo';
 import UIColors from '../../styling/variables/UIColors';
 import HamburgerButton from '../HamburgerButton';
 import { TopBarContainer, ItemContainer } from './topbar.styles';
+import { toggleSideMenu } from '../../actions/ui';
 
-const TopBar = props => {
+const TopBar = ({ ui, toggleSideMenu }) => {
   return (
     <TopBarContainer>
       <ItemContainer>
-        <HamburgerButton active />
+        <HamburgerButton active={ui.sideNavActive} click={toggleSideMenu} />
         <Logo fillColor={UIColors.light} svgWidth="3.2rem" svgHeigth="3.2rem" />
       </ItemContainer>
     </TopBarContainer>
   );
 };
 
-export default TopBar;
+export default connect(
+  state => ({
+    ui: state.ui,
+  }),
+  {
+    toggleSideMenu,
+  }
+)(TopBar);
