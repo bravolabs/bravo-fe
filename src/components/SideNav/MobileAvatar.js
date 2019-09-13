241d2274b239e4e2dd9f1bd2fa0615e0d40c05e2
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 06:39:06 +0100

    Add components to MobileAvatar

--- src/components/SideNav/MobileAvatar.js
+++ src/components/SideNav/MobileAvatar.js
@@ -3,6 +3,16 @@ import { Link } from 'react-router-dom';
 import { ExtraInfo, ComponentTitle } from '../../styling/atoms/Fonts';
 import { MobileAvatarContainer } from './sidenav.styles';
 
-const MobileAvatar = ({ src, alt, name, link }) => {};
+const MobileAvatar = ({ src, alt, name, link }) => {
+  return (
+    <Link to={link}>
+      <MobileAvatarContainer>
+        <img src={src} alt={alt} />
+        <ComponentTitle>{name}</ComponentTitle>
+        <ExtraInfo>View Profile</ExtraInfo>
+      </MobileAvatarContainer>
+    </Link>
+  );
+};
 
 export default MobileAvatar;
