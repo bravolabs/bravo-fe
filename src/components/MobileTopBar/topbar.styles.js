203638c1044a1e9ebfaa1d15f33fd8d60dce6c6e
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 02:19:11 +0100

    Add breakpoint to TopBarContainer

--- src/components/MobileTopBar/topbar.styles.js
+++ src/components/MobileTopBar/topbar.styles.js
@@ -2,4 +2,9 @@ import styled from 'styled-components';
 import UIColors from '../../styling/variables/UIColors';
 import Breakpoints from '../../styling/variables/Breakpoints';
 
-export const TopBarContainer = styled.nav``;
+export const TopBarContainer = styled.nav`
+  display: none;
+
+  @media (max-width: ${Breakpoints.medium}px) {
+  }
+`;
