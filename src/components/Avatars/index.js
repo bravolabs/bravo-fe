ff2a10cbc119002340ce9204059adc82a05b567b
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 15:34:14 +0100

    Render DefaultAvatar when avatar fails

--- src/components/Avatars/index.js
+++ src/components/Avatars/index.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
+import DefaultAvatar from './DefaultAvatar';
 
 const Avatar = props => {
-  const { avatar } = props;
+  const { avatar, userId } = props;
   const [imgFailed, setImgFailed] = useState(false);
-}
+
+  return imgFailed ? (
+    <DefaultAvatar />
+  ) : (
+    <img src={`${avatar}?d=404`} alt={userId} onError={e => setImgFailed(true)} />
+  );
+};
 
 export default Avatar;
