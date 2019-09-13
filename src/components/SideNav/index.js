c487f727a2cb491b5e3337341b70de800e9278fb
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 06:43:05 +0100

    Render MobileAvatar in sidebar

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -7,6 +7,7 @@ import Avatar from './Avatar';
 import NavItems from './NavItems';
 import logoutIcon from '../../assets/logout-icon.svg';
 import NavItem from './NavItem';
+import MobileAvatar from './MobileAvatar';
 
 const SideNav = props => {
   const { slack } = props;
@@ -15,6 +16,14 @@ const SideNav = props => {
       <Link to="/">
         <img src={bravoWhite} alt="Bravo" />
       </Link>
+      {slack.user && (
+        <MobileAvatar
+          link="/profile"
+          name={slack.user.name}
+          src={slack.user.avatar}
+          alt={slack.user.name}
+        />
+      )}
       {slack.isLoggedIn && <NavItems />}
       {slack.installSuccess && <Avatar src={slack.user.avatar} alt="user-avatar" />}
       {slack.isLoggedIn && <NavItem icon={logoutIcon} label="Logout" link="/logout" />}
