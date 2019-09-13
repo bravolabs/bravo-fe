65eaaea03d23c65c6d2d5d0d220bb85c97f4b054
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 06:26:16 +0100

    Create and export MobileAvatar component

--- src/components/SideNav/MobileAvatar.js
+++ src/components/SideNav/MobileAvatar.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ExtraInfo } from '../../styling/atoms/Fonts';
+
+const MobileAvatar = ({ src, alt }) => {};
+
+export default MobileAvatar;
