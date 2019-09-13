13f768f4f8f3e036a3888755eeac761c7b8a7a8b
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-13 07:45:15 +0100

    Add ui reducers to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import slack from './slackReducer';
 import team from './teamReducer';
 import shoutouts from './shoutoutReducer';
 import users from './usersReducer';
+import ui from './uiReducer';
 
 const rootReducer = combineReducers({
   slack,
   team,
   shoutouts,
   users,
+  ui,
 });
 
 export default rootReducer;
