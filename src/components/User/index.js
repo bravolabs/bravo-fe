f84a59904ac9d438cd8df8dbfdc4c54076a916ea
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-16 13:32:29 +0100

    Start adapting components to use new avatar

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -4,7 +4,7 @@ import { ShoutoutUserAvatar, Username, UserContainer } from './user.styles';
 const User = ({ avatar, name, size, userId }) => {
   return (
     <UserContainer size={size ? size : null}>
-      <ShoutoutUserAvatar avatar={img} name={name} userId={userId} size={size ? size : null} />
+      <ShoutoutUserAvatar avatar={avatar} name={name} userId={userId} size={size ? size : null} />
       <Username fontWeight="500" size={size ? size : null}>
         {name}
       </Username>
