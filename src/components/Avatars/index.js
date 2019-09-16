dc10d66a289b637dd9fd5f8c3ccc793f5c6b574f
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-16 13:25:01 +0100

    Change shoutout user avatar to new avatar

--- src/components/Avatars/index.js
+++ src/components/Avatars/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import DefaultAvatar from './DefaultAvatar';
 
 const Avatar = props => {
-  const { avatar, userId } = props;
+  const { avatar, userId, name } = props;
   const [imgFailed, setImgFailed] = useState(false);
 
   return imgFailed ? (
@@ -11,7 +11,7 @@ const Avatar = props => {
     <img
       className="avatar user-avatar"
       src={`${avatar}?d=404`}
-      alt={userId}
+      alt={name}
       onError={e => setImgFailed(true)}
     />
   );
--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { ShoutoutUserAvatar, Username, UserContainer } from './user.styles';
 
-const User = ({ img, alt, name, size }) => {
+const User = ({ avatar, name, size, userId }) => {
   return (
     <UserContainer size={size ? size : null}>
-      <ShoutoutUserAvatar src={img} alt={alt} size={size ? size : null} />
+      <ShoutoutUserAvatar avatar={img} name={name} userId={userId} size={size ? size : null} />
       <Username fontWeight="500" size={size ? size : null}>
         {name}
       </Username>
