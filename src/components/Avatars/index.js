8587eb63a7cf2148886e453389471ed7c2a8162c
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-17 16:27:30 +0100

    Pass through className. Must remember this for the future as will cause many headaches

--- src/components/Avatars/index.js
+++ src/components/Avatars/index.js
@@ -6,10 +6,11 @@ const Avatar = props => {
   const [imgFailed, setImgFailed] = useState(false);
 
   return imgFailed ? (
-    <DefaultAvatar className="avatar default-avatar" ColorSeed={seed} />
+    <DefaultAvatar className={`avatar default-avatar ${props.className}`} ColorSeed={seed} />
   ) : (
     <img
       className="avatar user-avatar"
+      className={`avatar user-avatar ${props.className}`}
       src={`${avatar}?d=404`}
       alt={name}
       onError={e => setImgFailed(true)}
