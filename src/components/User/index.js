c056144ffde8e290c67b342d6dffcefa48333345
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-17 16:08:09 +0100

    Use name as seed in user

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { ShoutoutUserAvatar, Username, UserContainer } from './user.styles';
 
-const User = ({ avatar, name, size, userId }) => {
+const User = ({ avatar, name, size }) => {
   return (
     <UserContainer size={size ? size : null}>
-      <ShoutoutUserAvatar avatar={avatar} name={name} userId={userId} size={size ? size : null} />
+      <ShoutoutUserAvatar avatar={avatar} name={name} seed={name} size={size ? size : null} />
       <Username fontWeight="500" size={size ? size : null}>
         {name}
       </Username>
