03dae643eca4a437c4add58fd0e7bae927741bfc
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-17 13:05:14 +0100

    Fix margins on users

--- src/components/ShoutoutCard/shoutout.styles.js
+++ src/components/ShoutoutCard/shoutout.styles.js
@@ -81,6 +81,10 @@ export const AvatarsContainer = styled.div`
       flex-direction: column;
       justify-content: flex-start;
       align-items: center;
+      > img {
+        margin: 0;
+        margin-bottom: 8px;
+      }
     }
   }
 `;
