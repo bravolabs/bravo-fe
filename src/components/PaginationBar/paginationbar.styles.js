b829c87d347ed6ff9915de308d9f38dec454b24f
erozonachi <erozonachi.james@gmail.com>
2019-09-18 22:01:49 +0100

    Add common styles for button and span in container

--- src/components/PaginationBar/paginationbar.styles.js
+++ src/components/PaginationBar/paginationbar.styles.js
@@ -11,4 +11,18 @@ export const Container = styled.div`
   box-shadow: 0px 2px 2px #f2f3f7;
   background-color: white;
   margin-bottom: 20px;
+
+  button,
+  span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    height: 5rem;
+    border: none;
+    outline: none;
+    font-size: 2rem;
+    font-weight: 300;
+    color: #4265ed;
+  }
 `;
