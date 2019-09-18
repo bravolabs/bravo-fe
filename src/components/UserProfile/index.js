7fc76c605713f8cc976da01971440f6d6d42fe8f
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-18 17:26:53 +0100

    Fix ref issue using state

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -21,7 +21,7 @@ const shoutoutMargin = '20px auto 15px 200px';
 const UserProfile = ({ user, shoutouts, fetching, message }) => {
   const initShoutouts =
     shoutouts && shoutouts.filter(item => item.giverName.toLowerCase() === user.name.toLowerCase());
-
+  const [ navRef, setNavRef ] = useState(React.createRef());
   const [state, setState] = useState({
     active: 'given',
     shoutouts: initShoutouts,
@@ -52,10 +52,8 @@ const UserProfile = ({ user, shoutouts, fetching, message }) => {
     }));
   }, [shoutouts, active, name]);
 
-  const navigationRef = React.createRef();
-
   const handleScroll = e => {
-    if (window.pageYOffset >= navigationRef.current.offsetTop) {
+    if (window.pageYOffset >= navRef.current.scrollTop) {
       setState(prevState => ({
         ...prevState,
         navSticky: true,
@@ -84,7 +82,7 @@ const UserProfile = ({ user, shoutouts, fetching, message }) => {
           alt={user && user.name}
           name={user && user.name}
         />
-        <ProfileNavigation innerRef={navigationRef}>
+        <ProfileNavigation ref={navRef}>
           <ShoutoutsButton
             active={state.active === 'given' ? true : false}
             onClick={() => handleClick('given')}>
