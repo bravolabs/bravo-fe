6e6cf176a67230d0d4f4a9e6f8397f37046f6316
erozonachi <erozonachi.james@gmail.com>
2019-09-18 21:49:13 +0100

    Add styles to container styled component

--- src/components/PaginationBar/paginationbar.styles.js
+++ src/components/PaginationBar/paginationbar.styles.js
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
+  height: 60px;
+  border-radius: 30px;
+  box-sizing: border-box;
+  box-shadow: 0px 2px 2px #f2f3f7;
+  background-color: white;
+  margin-bottom: 20px;
+`;
