3817b19efc05ea82adc643964bc2342b1b6629a8
erozonachi <erozonachi.james@gmail.com>
2019-09-18 22:17:27 +0100

    Import and user container component

--- src/components/PaginationBar/index.js
+++ src/components/PaginationBar/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Container } from './paginationbar.styles';
 
-const PaginationBar = ({ fnPrevious, fnNext, currentPage }) => <></>;
+const PaginationBar = ({ fnPrevious, fnNext, currentPage }) => (
+  <Container>
+    <button onClick={fnPrevious}>Previous</button>
+    <span>{currentPage}</span>
+    <button onClick={fnNext}>Next</button>
+  </Container>
+);
 
 export default PaginationBar;
