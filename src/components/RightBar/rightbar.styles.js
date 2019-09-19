9a4f7cc390bdd218b4338ee87bae5859197662f7
Noble Obioma <nobioma1@gmail.com>
2019-09-19 18:05:58 +0100

    Fix breakpoint

--- src/components/RightBar/rightbar.styles.js
+++ src/components/RightBar/rightbar.styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { Card } from '../../styling/atoms/Card';
 import colors from '../../styling/variables/UIColors';
+import Breakpoints from '../../styling/variables/Breakpoints';
 
 export const RightBarContainer = styled.div`
   display: flex;
@@ -15,7 +16,7 @@ export const RightBarContainer = styled.div`
     flex-direction: column;
   }
 
-  @media (max-width: 700px) {
+  @media (max-width: ${Breakpoints.large}px) {
     display: none;
   }
 `;
