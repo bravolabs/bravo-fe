d07f1879e5d325fe4d2af78664b18e82a0f98fda
Noble Obioma <nobioma1@gmail.com>
2019-09-19 15:58:31 +0100

    Remove username style properties and have font-size and weight, Remove margin from User container

--- src/components/User/user.styles.js
+++ src/components/User/user.styles.js
@@ -4,21 +4,17 @@ import Avatar from '../Avatars';
 
 export const UserContainer = styled.div`
   display: flex;
-  margin: ${props => (props.size === 'user-profile' ? '10px auto auto 0' : 'none')};
+  align-items: center;
 `;
 
 export const ShoutoutUserAvatar = styled(Avatar)`
   margin: ${props => (props.size === 'user-profile' ? 'auto 20px auto 0' : 'auto 12px auto 0')};
-  width: ${props => (props.size === 'user-profile' ? '140px' : '32px')};
-  height: ${props => (props.size === 'user-profile' ? '140px' : '32px')};
+  width: ${props => (props.size === 'user-profile' ? '100px' : '32px')};
+  height: ${props => (props.size === 'user-profile' ? '100px' : '32px')};
   border-radius: 50%;
 `;
 
 export const Username = styled(ComponentTitle)`
-  margin: auto 0;
-  vertical-align: center;
-  width: fit-content;
-  height: ${props => (props.size === 'user-profile' ? '3.5rem' : '2rem')};
-  line-height: ${props => (props.size === 'user-profile' ? '3.2rem' : '1.9rem')};
-  font-size: ${props => props.size === 'user-profile' && '32px'};
+  font-size: ${props => props.size === 'user-profile' && '2.8rem'};
+  font-weight: 300;
 `;
