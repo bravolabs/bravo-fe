53e2586a1f31c82bc450a8e2acacf4caf44b001a
Noble Obioma <nobioma1@gmail.com>
2019-09-19 15:56:18 +0100

    Remove overlay veiew container effect

--- src/components/ShoutoutCard/ShoutoutCard.js
+++ src/components/ShoutoutCard/ShoutoutCard.js
@@ -1,48 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {
   ShoutoutCardContainer,
   Emoji,
   AvatarsContainer,
   Time,
   ShoutoutText,
-  ViewContainer,
-  ViewButton,
 } from './shoutout.styles';
 import User from '../User';
 import confetti from '../../assets/confetti.png';
 import placeholder from '../../assets/placeholder.png';
 
 const ShoutoutCard = props => {
-  const {
-    id,
-    margin,
-    praiseGiver,
-    giverAvatar,
-    receiverAvatar,
-    praiseTaker,
-    time,
-    praiseText,
-  } = props;
-
-  const addViewAction = () => {
-    if (id) {
-      document.querySelector(`#view-${id}`).style.display = 'flex';
-    }
-  };
-  const removeViewAction = () => {
-    if (id) {
-      document.querySelector(`#view-${id}`).style.display = 'none';
-    }
-  };
+  const { praiseGiver, giverAvatar, receiverAvatar, praiseTaker, time, praiseText, hover } = props;
 
   return (
-    <ShoutoutCardContainer
-      margin={margin}
-      onMouseEnter={addViewAction}
-      onClick={addViewAction}
-      onBlur={removeViewAction}
-      onMouseLeave={removeViewAction}>
+    <ShoutoutCardContainer hover={hover}>
       <AvatarsContainer>
         <User avatar={giverAvatar || placeholder} name={praiseGiver} />
         <Emoji src={confetti} />
@@ -51,13 +23,6 @@ const ShoutoutCard = props => {
       </AvatarsContainer>
       <ShoutoutText>{praiseText}</ShoutoutText>
       <Time className="mobile-time">{time}</Time>
-      {id && (
-        <ViewContainer id={`view-${id}`}>
-          <ViewButton>
-            <Link to={`/shoutouts/${id}`}>View shoutout</Link>
-          </ViewButton>
-        </ViewContainer>
-      )}
     </ShoutoutCardContainer>
   );
 };
