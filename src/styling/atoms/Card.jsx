5121f47eee20b18029cdffc052d27e66b92c7d79
Noble Obioma <nobioma1@gmail.com>
2019-09-19 16:43:10 +0100

    Add padding top to card

--- src/styling/atoms/Card.jsx
+++ src/styling/atoms/Card.jsx
@@ -4,7 +4,7 @@ export const Card = styled.div`
   font-size: ${props => (props.fontSize ? props.fontSize : '1.6rem')};
   color: ${props => (props.color ? props.color : 'black')};
   font-weight: ${props => (props.fontWeight ? props.fontWeight : '300')};
-  padding: 24px;
+  padding: 16px 24px;
   border-radius: ${props => (props.radius ? props.radius : '4px')};
   ${props => (props.shadow ? `box-shadow: ${props.shadow};` : null)};
   background-color: white;
