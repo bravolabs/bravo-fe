269ec669cc5e1d06cb84fb0316ec9ad3ab59f315
Noble Obioma <nobioma1@gmail.com>
2019-09-19 17:50:01 +0100

    Create Main content style component for dashboard

---
+++ src/views/Dashboard/dashboard.styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import Breakpoints from '../../styling/variables/Breakpoints';
+
+export const MainContent = styled.div`
+  display: flex;
+  margin-left: 90px;
+  padding: 0 32px;
+  height: 100vh;
+
+  & > section {
+    height: 100vh;
+    width: 950px;
+    overflow: auto;
+    padding-right: 25px;
+  }
+
+  @media (max-width: ${Breakpoints.medium}px) {
+    max-width: 100vw;
+    margin-left: 0;
+    padding: 0;
+    left: 0;
+    right: 0;
+
+    & > section {
+      padding: 5px;
+    }
+  }
+`;
