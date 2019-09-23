3536c75608d0d858f121c7487bcf7e402471c64f
ogwurujohnson <ogwurujohnson@gmail.com>
2019-09-23 11:57:08 +0100

    create styled component export component for search bar

---
+++ src/components/Search/search.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SearchWrapper = styled.div`
+    background: #ffffff;
+    box-shadow: 0px 1.87781px 6.25935px rgba(0, 0, 0, 0.06);
+    border-radius: 100px;
+    width: 220px;
+    height: 35px;
+`;
