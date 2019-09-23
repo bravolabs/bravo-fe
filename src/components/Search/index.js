bf0ca6df39e82f2b7b38d771c1606bf5ce3123a6
ogwurujohnson <ogwurujohnson@gmail.com>
2019-09-23 11:56:10 +0100

    Chore:  Style search bar

---
+++ src/components/Search/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SearchWrapper } from './search.styles';
+
+const Search = () => {
+    return (
+        <React.Fragment>
+            <SearchWrapper>
+                
+                <p>Search</p>
+            </SearchWrapper>
+        </React.Fragment>
+    )
+}
+
+export default Search;
