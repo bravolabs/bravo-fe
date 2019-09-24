0352038eda7af147d9b7d911e17e495ac2ab0fc8
Awa Desmoline <awadesmoline@gmail.com>
2019-09-24 09:11:15 +0100

    format code

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SearchWrapper, TextWrapper } from './search.styles';
-import { IoMdSearch } from "react-icons/io";
+import { IoMdSearch } from 'react-icons/io';
 
 const Search = () => {
-    return (
-        <React.Fragment>
-            <SearchWrapper>
-                <TextWrapper>
-                    <IoMdSearch className="icon"/>
-                    <input placeholder="Search"/>
-                </TextWrapper>
-            </SearchWrapper>
-        </React.Fragment>
-    )
-}
+  const [searchTerm, setSearchTerm] = useState(null);
+  return (
+    <React.Fragment>
+      <SearchWrapper>
+        <TextWrapper>
+          <IoMdSearch className="icon" />
+          <input placeholder="Search" />
+        </TextWrapper>
+      </SearchWrapper>
+    </React.Fragment>
+  );
+};
 
 export default Search;
