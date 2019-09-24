ba584c94c5d776ade61a558e7781003e038844c8
Awa Desmoline <awadesmoline@gmail.com>
2019-09-24 11:05:19 +0100

    chore: create and handle form inputs

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import { SearchWrapper, TextWrapper } from './search.styles';
 import { IoMdSearch } from 'react-icons/io';
+import Fuse from 'fuse.js';
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = useState(null);
+const Search = ({ searchArray }) => {
+  console.log(searchArray);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(searchTerm);
+    setSearchTerm('');
+  };
   return (
     <React.Fragment>
       <SearchWrapper>
         <TextWrapper>
           <IoMdSearch className="icon" />
-          <input placeholder="Search" />
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="Search"
+              value={searchTerm}
+              onChange={e => setSearchTerm(e.target.value)}
+            />
+          </form>
         </TextWrapper>
       </SearchWrapper>
     </React.Fragment>
